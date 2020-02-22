4fe00952a920664acd8e23033e0840dea71176cd
ooobsidian <obsidian_lrx@aliyun.com>
2020-02-22 17:26:13 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test1.dir/main.cpp.o"
+  "test1.pdb"
+  "test1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
